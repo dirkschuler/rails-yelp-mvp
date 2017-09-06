cb92d06620f9f1affea0908c3b7ac89b9d2305e3
Dirk Schuler <dscbdv@gmail.com>
2017-09-06 14:48:46 +0200

    Restaurant Review exercise complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  "123",
+    category:       "french"
+ },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "456",
+    category:       "french"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:  "789",
+    category:       "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
